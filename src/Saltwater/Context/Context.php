0b2b6fed7eade15161a2646859e5693595109e59
David Deutsch <skore@valanx.org>
2014-04-05 00:55:49 +0200

    cleanup, better fallback rest service

--- src/Saltwater/Context/Context.php
+++ src/Saltwater/Context/Context.php
@@ -43,18 +43,10 @@ class Context
 		if ( class_exists($class) ) return $class;
 
 		if ( in_array($name, $this->services) ) {
-			$class = $this->namespace . '\Service\Rest';
-
-			if ( !class_exists($class) ) {
-				$class = $this->namespace . '\Service\Rest';
-			}
-
+			return 'Saltwater\Service\Rest';
 		} else {
 			return '';
 		}
-
-		return $class;
-
 	}
 
 	public function getService( $service, $result )
@@ -77,38 +69,6 @@ class Context
 		} else {
 			return $this->parent->getDB();
 		}
-	}
-
-	public function db()
-	{
-		if ( empty(self::$r) ) {
-			self::$r = new \RedBean_Instance();
-		}
-
-		$cfg = self::$config->database;
-
-		if ( empty(self::$r->toolboxes) ) {
-			self::$r->setup(
-				'mysql:host=' . $cfg->host . ';' . 'dbname=' . $cfg->name,
-				$cfg->user,
-				$cfg->password
-			);
-
-			self::$r->setupPipeline();
-		}
-
-		if ( !isset(self::$r->toolboxes[$cfg->name]) ) {
-			self::$r->addDatabase(
-				$cfg->name,
-				'mysql:host=' . $cfg->host . ';' . 'dbname=' . $cfg->name,
-				$cfg->user,
-				$cfg->password
-			);
-		}
-
-		self::$r->selectDatabase($cfg->name);
-
-		self::$r->useWriterCache(true);
 	}
 
 	public function getInfo()
