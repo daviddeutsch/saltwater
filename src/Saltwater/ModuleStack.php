be79b2f460f4b47f557423430e18c3d33dae8556
David Deutsch <skore@valanx.org>
2014-05-13 00:58:46 +0200

    refactoring to decouple module stack from navigator

---
+++ src/Saltwater/ModuleStack.php
@@ -0,0 +1,342 @@
+<?php
+
+namespace Saltwater;
+
+use Saltwater\Server as S;
+use Saltwater\Utils as U;
+
+class ModuleStack extends \ArrayIterator
+{
+	/**
+	 * @var string
+	 */
+	private $root = 'root';
+
+	/**
+	 * @var string
+	 */
+	private $master = '';
+
+	/**
+	 * @var string[] array of modules stacked in the order they were called in
+	 */
+	private $stack = array();
+
+	/**
+	 * @return \Saltwater\Thing\Module
+	 */
+	public function current()
+	{
+		return parent::current();
+	}
+
+	/**
+	 * Set the root module by name
+	 *
+	 * @param string $name
+	 */
+	public function setRoot( $name )
+	{
+		if ( empty($name) || ($name == $this->root) ) return;
+
+		$this->root = $name;
+	}
+
+	/**
+	 * Set the master module by name
+	 *
+	 * @param string $name
+	 */
+	public function setMaster( $name )
+	{
+		if ( empty($name) || ($name == $this->master) ) return;
+
+		$this->master = $name;
+
+		$this->pushStack($name);
+	}
+
+	/**
+	 * Push a module name onto the stack, establishing later hierarchy for calls
+	 *
+	 * @param string $name
+	 */
+	private function pushStack( $name )
+	{
+		if ( empty($this->stack) ) $this->stack[] = $this->root;
+
+		if ( in_array($name, $this->stack) ) return;
+
+		$this->stack[] = $name;
+	}
+
+	/**
+	 * Add module to stack and register its things
+	 *
+	 * @param string $class  Full Classname
+	 * @param bool   $master true if this is also the master module
+	 *
+	 * @return bool|null
+	 */
+	public function appendModule( $class, $master=false )
+	{
+		if ( !class_exists($class) ) return false;
+
+		$name = U::namespacedClassToDashed($class);
+
+		if ( isset($this[$name]) ) return null;
+
+		$module = $this->moduleInstance($class);
+
+		$module->register($name);
+
+		// Push to ->modules late to preserve dependency order
+		$this[$name] = $module;
+
+		if ( $master ) $this->setMaster($name);
+
+		return true;
+	}
+
+	/**
+	 * @param $class
+	 *
+	 * @return \Saltwater\Thing\Module
+	 */
+	private function moduleInstance( $class )
+	{
+		return new $class();
+	}
+
+	/**
+	 * Return the master context for the current master module
+	 *
+	 * @param \Saltwater\Thing\Context|null $parent inject a parent context
+	 *
+	 * @return \Saltwater\Thing\Context
+	 */
+	public function masterContext( $parent=null )
+	{
+		$this->rewind();
+
+		foreach ( $this as $name => $module ) {
+			$context = $module->masterContext();
+
+			if ( !empty($context) ) {
+				$parent = S::$n->context->get($context, $parent);
+			}
+
+			if ( $name == $this->master ) break;
+		}
+
+		return $parent;
+	}
+
+	/**
+	 * Get the Module that provides a context
+	 *
+	 * @param string $name plain name of the context
+	 *
+	 * @return Thing\Module|null
+	 */
+	public function getContextModule( $name )
+	{
+		$bit = S::$n->bitThing('context.' . $name);
+
+		$this->rewind();
+
+		foreach ( $this as $module ) {
+			if ( $module->hasThing($bit) ) return $module;
+		}
+
+		return null;
+	}
+
+	/**
+	 * @param int    $bit
+	 * @param string $caller
+	 * @param string $type
+	 *
+	 * @return bool|Thing\Provider
+	 */
+	public function providerFromModule( $bit, $caller, $type)
+	{
+		// Depending on the caller, reset the module stack
+		$this->setMaster($caller);
+
+		foreach ( $this->modulePrecedence() as $k ) {
+			if ( !$this[$k]->hasThing($bit) ) continue;
+
+			$return = $this[$k]->provider($k, $caller, $type);
+
+			if ( $return !== false ) return $return;
+		}
+
+		return $this->tryModuleFallback($bit, $type);
+	}
+
+	private function tryModuleFallback( $bit, $type )
+	{
+		$master = array_search($this->master, $this->stack);
+
+		if ( $master == (count($this->stack) - 1) ) return false;
+
+		// As a last resort, step one module up within stack and try again
+		$caller = $this->stack[$master+1];
+
+		return $this->providerFromModule($bit, $caller, $type);
+	}
+
+	/**
+	 * Find the module of a caller class
+	 *
+	 * @param array|null $caller
+	 * @param string     $provider
+	 *
+	 * @return string module name
+	 */
+	public function findModule( $caller, $provider )
+	{
+		if ( empty($caller) ) return null;
+
+		$caller = $this->explodeCaller($caller, $provider);
+
+		$bit = S::$n->bitThing($caller->thing);
+
+		$this->rewind();
+
+		foreach ( array_reverse($this) as $k => $module ) {
+			if ( !$this->bitInModule($module, $caller, $bit) ) continue;
+
+			return $k;
+		}
+
+		return null;
+	}
+
+	/**
+	 * @param Thing\Module $module
+	 * @param object       $c
+	 * @param string       $bit
+	 *
+	 * @return bool
+	 */
+	private function bitInModule( $module, $c, $bit )
+	{
+		// A provider calling itself always gets a lower level provider
+		if ( $c->is_provider && ($module->namespace == $c->namespace) ) {
+			return false;
+		} elseif ( !$c->is_provider && ($module->namespace != $c->namespace) ) {
+			return false;
+		}
+
+		return $module->hasThing($bit);
+	}
+
+	/**
+	 * @param array  $caller
+	 * @param string $provider
+	 *
+	 * @return object
+	 */
+	private function explodeCaller( $caller, $provider )
+	{
+		// Extract a thing from the last two particles
+		$class = array_pop($caller);
+
+		$thing = strtolower( array_pop($caller) . '.' . $class );
+
+		// The rest is the namespace
+		return (object) array(
+			'thing'        => $thing,
+			'namespace'    => implode('\\', $caller),
+			'is_provider'  => $thing == $provider
+		);
+	}
+
+	/**
+	 * Return top candidate Module for providing a Thing
+	 *
+	 * @param string $thing
+	 * @param bool   $precedence Use the current module precedence rules
+	 *
+	 * @return bool|mixed
+	 */
+	public function moduleByThing( $thing, $precedence=true )
+	{
+		return $this->modulesByThing($thing, $precedence, true);
+	}
+
+	/**
+	 * Return a list of Modules providing a Thing
+	 * @param      string $thing
+	 * @param bool $precedence
+	 * @param bool $first      only return the first item on the list
+	 *
+	 * @return array|bool
+	 */
+	public function modulesByThing( $thing, $precedence=true, $first=false )
+	{
+		if ( !S::$n->isThing($thing) ) return false;
+
+		if ( $precedence ) {
+			$modules = $this->modulePrecedence();
+		} else {
+			$this->rewind();
+
+			$modules = array_keys( array_reverse($this) );
+		}
+
+		$bit = S::$n->bitThing($thing);
+
+		return $this->thingInStack($modules, $bit, $first);
+	}
+
+	private function thingInStack( $modules, $bit, $first )
+	{
+		$return = array();
+		foreach ( $modules as $module ) {
+			if ( !$this[$module]->hasThing($bit) ) continue;
+
+			if ( $first ) return $module;
+
+			$return[] = $module;
+		}
+
+		return $return;
+	}
+
+	private function modulePrecedence()
+	{
+		$return = array();
+		foreach ( $this->stack as $module ) {
+			array_unshift($return, $module);
+
+			if ( $module == $this->master ) break;
+		}
+
+		return $return;
+	}
+
+	public function __sleep()
+	{
+		$this->rewind();
+
+		foreach ( $this as $k => $v ) {
+			$this[$k] = array(
+				'class' => get_class($v),
+				'things' => $v->things
+			);
+		}
+	}
+
+	public function __wakeup()
+	{
+		foreach ( $this as $k => $v ) {
+			$class = $v['class'];
+
+			$this[$k] = new $class();
+			$this[$k]->things = $v['things'];
+		}
+	}
+}
--- src/Saltwater/Navigator.php
+++ src/Saltwater/Navigator.php
@@ -27,7 +27,7 @@ use Saltwater\Utils as U;
 class Navigator
 {
 	/**
-	 * @var Thing\Module[]
+	 * @var ModuleStack
 	 */
 	private $modules = array();
 
@@ -37,21 +37,6 @@ class Navigator
 	private $things = array();
 
 	/**
-	 * @var string
-	 */
-	private $root = 'root';
-
-	/**
-	 * @var string
-	 */
-	private $master = '';
-
-	/**
-	 * @var string[] array of modules stacked in the order they were called in
-	 */
-	private $stack = array();
-
-	/**
 	 * @var array classes that can be skipped during search for caller module
 	 */
 	private $skip = array(
@@ -59,32 +44,17 @@ class Navigator
 		'Saltwater\Server'
 	);
 
+	public function __construct()
+	{
+		$this->modules = new ModuleStack();
+	}
+
 	/**
-	 * Add module to Navigator and register its things
-	 *
-	 * @param string $class  Full Classname
-	 * @param bool   $master true if this is also the master module
-	 *
-	 * @return bool|null
+	 * @see ModuleStack::appendModule()
 	 */
 	public function addModule( $class, $master=false )
 	{
-		if ( !class_exists($class) ) return false;
-
-		$name = U::namespacedClassToDashed($class);
-
-		if ( isset($this->modules[$name]) ) return null;
-
-		$module = new $class();
-
-		$module->register($name);
-
-		// Push to ->modules late to preserve dependency order
-		$this->modules[$name] = $module;
-
-		if ( $master ) $this->setMaster($name);
-
-		return true;
+		return $this->modules->appendModule($class, $master);
 	}
 
 	/**
@@ -92,15 +62,11 @@ class Navigator
 	 */
 	public function cache( $path )
 	{
-		$cache = new \stdClass();
-
-		$this->copyCache( $this, $cache );
-
 		$info = pathinfo($path);
 
 		if ( !is_dir($info['dirname']) ) mkdir($info['dirname'], 0744, true);
 
-		file_put_contents( $path, serialize($cache)	);
+		file_put_contents( $path, serialize($this) );
 	}
 
 	/**
@@ -112,35 +78,8 @@ class Navigator
 	{
 		$cache = unserialize( file_get_contents($path) );
 
-		return $this->copyCache( $cache, $this );
-	}
-
-	/**
-	 * @param Navigator|object $from
-	 * @param Navigator|object $to
-	 *
-	 * @return bool
-	 */
-	private function copyCache( $from, &$to )
-	{
-		foreach ( array('things', 'root', 'master', 'stack') as $k ) {
-			$to->$k = $from->$k;
-		}
-
-		$nav = $to instanceof Navigator;
-
-		if ( !$nav ) $to->modules = array();
-
-		foreach ( $from->modules as $name => $module ) {
-			if ( $nav ) {
-				$to->modules[$name] = new $module();
-
-				$to->modules[$name]->things = $from->bits[$name];
-			} else {
-				$to->modules[$name] = get_class($module);
-
-				$to->bits[$name] = $module->things;
-			}
+		foreach ( $cache as $k => $v ) {
+			$this->$k = $v;
 		}
 
 		return true;
@@ -190,22 +129,6 @@ class Navigator
 	/**
 	 * Return a module class by its name
 	 *
-	 * @param bool $reverse
-	 *
-	 * @return Thing\Module[]
-	 */
-	public function getModules( $reverse=false )
-	{
-		if ( $reverse ) {
-			return array_reverse($this->modules);
-		} else {
-			return $this->modules;
-		}
-	}
-
-	/**
-	 * Return a module class by its name
-	 *
 	 * @param string $name
 	 *
 	 * @return Thing\Module
@@ -216,83 +139,19 @@ class Navigator
 	}
 
 	/**
-	 * Return the master context for the current master module
-	 *
-	 * @param \Saltwater\Thing\Context|null $parent inject a parent context
-	 *
-	 * @return \Saltwater\Thing\Context
+	 * @see ModuleStack::masterContext()
 	 */
 	public function masterContext( $parent=null )
 	{
-		foreach ( $this->modules as $name => $module ) {
-			$context = $module->masterContext();
-
-			if ( !empty($context) ) {
-				$parent = $this->context->get($context, $parent);
-			}
-
-			if ( $name == $this->master ) break;
-		}
-
-		return $parent;
+		return $this->modules->masterContext($parent);
 	}
 
 	/**
-	 * Get the Module that provides a context
-	 *
-	 * @param string $name plain name of the context
-	 *
-	 * @return Thing\Module|null
+	 * @see ModuleStack::getContextModule()
 	 */
 	public function getContextModule( $name )
 	{
-		$bit = $this->bitThing('context.' . $name);
-
-		foreach ( $this->modules as $module ) {
-			if ( $module->hasThing($bit) ) return $module;
-		}
-
-		return null;
-	}
-
-	/**
-	 * Set the root module by name
-	 *
-	 * @param string $name
-	 */
-	public function setRoot( $name )
-	{
-		if ( empty($name) || ($name == $this->root) ) return;
-
-		$this->root = $name;
-	}
-
-	/**
-	 * Set the master module by name
-	 *
-	 * @param string $name
-	 */
-	public function setMaster( $name )
-	{
-		if ( empty($name) || ($name == $this->master) ) return;
-
-		$this->master = $name;
-
-		$this->pushStack($name);
-	}
-
-	/**
-	 * Push a module name onto the stack, establishing later hierarchy for calls
-	 *
-	 * @param string $name
-	 */
-	private function pushStack( $name )
-	{
-		if ( empty($this->stack) ) $this->stack[] = $this->root;
-
-		if ( in_array($name, $this->stack) ) return;
-
-		$this->stack[] = $name;
+		return $this->modules->getContextModule($name);
 	}
 
 	/**
@@ -312,110 +171,10 @@ class Navigator
 		};
 
 		if ( empty($caller) ) {
-			$caller = $this->findModule($this->lastCaller(), $thing);
+			$caller = $this->modules->findModule($this->lastCaller(), $thing);
 		}
 
-		return $this->providerFromModule($bit, $caller, $type);
-	}
-
-	/**
-	 * @param int    $bit
-	 * @param string $caller
-	 * @param string $type
-	 *
-	 * @return bool|Thing\Provider
-	 */
-	private function providerFromModule( $bit, $caller, $type)
-	{
-		// Depending on the caller, reset the module stack
-		$this->setMaster($caller);
-
-		foreach ( $this->modulePrecedence() as $k ) {
-			if ( !$this->modules[$k]->hasThing($bit) ) continue;
-
-			$return = $this->modules[$k]->provider($k, $caller, $type);
-
-			if ( $return !== false ) return $return;
-		}
-
-		return $this->tryModuleFallback($bit, $type);
-	}
-
-	private function tryModuleFallback( $bit, $type )
-	{
-		$master = array_search($this->master, $this->stack);
-
-		if ( $master == (count($this->stack) - 1) ) return false;
-
-		// As a last resort, step one module up within stack and try again
-		$caller = $this->stack[$master+1];
-
-		return $this->providerFromModule($bit, $caller, $type);
-	}
-
-	/**
-	 * Find the module of a caller class
-	 *
-	 * @param array|null $caller
-	 * @param string     $provider
-	 *
-	 * @return string module name
-	 */
-	private function findModule( $caller, $provider )
-	{
-		if ( empty($caller) ) return null;
-
-		$caller = $this->explodeCaller($caller, $provider);
-
-		$bit = $this->bitThing($caller->thing);
-
-		foreach ( $this->getModules(true) as $k => $module ) {
-			if ( !$this->bitInModule($module, $caller, $bit) ) continue;
-
-			return $k;
-		}
-
-		return null;
-	}
-
-	/**
-	 * @param Thing\Module $module
-	 * @param object       $c
-	 * @param string       $bit
-	 *
-	 * @return bool
-	 */
-	private function bitInModule( $module, $c, $bit )
-	{
-		// A provider calling itself always gets a lower level provider
-		if ( $c->is_provider && ($module->namespace == $c->namespace) ) {
-			return false;
-		} elseif ( !$c->is_provider && ($module->namespace != $c->namespace) ) {
-			return false;
-		}
-
-		return $module->hasThing($bit);
-	}
-
-	/**
-	 * @param array  $caller
-	 * @param string $provider
-	 *
-	 * @return object
-	 */
-	private function explodeCaller( $caller, $provider )
-	{
-		// Extract a thing from the last two particles
-		$class = array_pop($caller);
-
-		$thing = strtolower( array_pop($caller) . '.' . $class );
-
-		// The rest is the namespace
-		return (object) array(
-			'thing'        => $thing,
-			'namespace'    => implode('\\', $caller),
-			'is_provider'  => $thing == $provider
-		);
+		return $this->modules->providerFromModule($bit, $caller, $type);
 	}
 
 	/**
@@ -450,68 +209,6 @@ class Navigator
 		return (strpos($class, 'Saltwater\Root') !== false)
 			|| (strpos($class, '\\') === false)
 			|| in_array($class, $this->skip);
-	}
-
-	/**
-	 * Return top candidate Module for providing a Thing
-	 *
-	 * @param string $thing
-	 * @param bool   $precedence Use the current module precedence rules
-	 *
-	 * @return bool|mixed
-	 */
-	public function moduleByThing( $thing, $precedence=true )
-	{
-		return $this->modulesByThing($thing, $precedence, true);
-	}
-
-	/**
-	 * Return a list of Modules providing a Thing
-	 * @param      string $thing
-	 * @param bool $precedence
-	 * @param bool $first      only return the first item on the list
-	 *
-	 * @return array|bool
-	 */
-	public function modulesByThing( $thing, $precedence=true, $first=false )
-	{
-		if ( !$this->isThing($thing) ) return false;
-
-		if ( $precedence ) {
-			$modules = $this->modulePrecedence();
-		} else {
-			$modules = array_keys( $this->getModules(true) );
-		}
-
-		$bit = $this->bitThing($thing);
-
-		return $this->thingInStack($modules, $bit, $first);
-	}
-
-	private function thingInStack( $modules, $bit, $first )
-	{
-		$return = array();
-		foreach ( $modules as $module ) {
-			if ( !$this->modules[$module]->hasThing($bit) ) continue;
-
-			if ( $first ) return $module;
-
-			$return[] = $module;
-		}
-
-		return $return;
-	}
-
-	private function modulePrecedence()
-	{
-		$return = array();
-		foreach ( $this->stack as $module ) {
-			array_unshift($return, $module);
-
-			if ( $module == $this->master ) break;
-		}
-
-		return $return;
 	}
 
 	public function __get( $type )
