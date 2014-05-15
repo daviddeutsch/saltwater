275b1243ac74cb31d78e989c02988f95a181efde
David Deutsch <skore@valanx.org>
2014-05-16 01:44:53 +0200

    preparing navigator test

---
+++ dev/tests/NavigatorTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use Saltwater\Navigator;
+
+class TempStackTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * @var Navigator
+	 */
+	public $n;
+
+	public function setUp()
+	{
+		$this->n = new Navigator;
+	}
+
+	public function testNewNavigator()
+	{
+
+	}
+
+}
