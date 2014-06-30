1d78e3ba625d05417ea62e3e5a9452baa9b22dbe
David Deutsch <skore@valanx.org>
2014-07-01 00:44:07 +0200

    capturing all coverage submissions, testing more ModuleFinder stuff

---
+++ dev/suites/core/ModuleFinderTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use Saltwater\Server as S;
+
+class ModuleFinderTest extends \PHPUnit_Framework_TestCase
+{
+	public static function setUpBeforeClass()
+	{
+		S::destroy();
+
+		S::init('Saltwater\Test\Test');
+	}
+
+	protected function tearDown()
+	{
+		S::destroy();
+	}
+
+	public function testFinderOutliers()
+	{
+		$bit = S::$n->registry->bit('provider.response');
+
+		$this->assertEquals(
+			array('app', 'test'),
+			S::$n->modules->finder->getSaltModules($bit)
+		);
+
+		$this->assertEmpty( S::$n->modules->finder->getSaltModules(1234) );
+	}
+}
